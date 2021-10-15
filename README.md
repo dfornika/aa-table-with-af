eee5a4c09b7d4f8b9fc620fb75a48923abe22d73
Dan Fornika <dfornika@gmail.com>
2021-10-15 12:08:14 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# aa-table-with-af
+Produce an 'amino acid table' file from a vcf, including depth and alt frequency info.
+
+## Usage
+```
+usage: aa_table_with_af.py [-h] [-s SAMPLE_ID] vcf
+
+positional arguments:
+  vcf
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s SAMPLE_ID, --sample_id SAMPLE_ID
+```
---
+++ aa_table_with_af.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import pysam
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vcf')
+    parser.add_argument('-s', '--sample_id')
+    args = parser.parse_args()
+
+    vcf = pysam.VariantFile(args.vcf)
+
+    output_fields = [
+        'sample',
+        'chr',
+        'pos',
+        'ref',
+        'alt',
+        'depth',
+        'alt_frequency',
+        'consequence',
+        'gene',
+        'protein',
+        'aa',
+    ]
+
+    print('\t'.join(output_fields))
+
+    for rec in vcf.fetch():
+        output = []
+        if args.sample_id:
+            sample_id = args.sample_id
+        else:
+            sample_id = rec.samples[0].name
+        output.append(sample_id)
+        output.append(rec.chrom)
+        output.append(rec.pos)
+        output.append(rec.ref)
+        output.append(rec.alts[0])
+        for s in rec.samples.items():
+            output.append(s[1]['DP'])
+        output.append(rec.info['AF'][0])
+        if 'BCSQ' in rec.info:
+            bcsq = rec.info['BCSQ'][0].split('|')
+            consequence = bcsq[0]
+            gene = bcsq[1]
+            aa = bcsq[5]
+            output.append(consequence)
+            output.append(gene)
+            output.append(aa)
+            output.append('-'.join([gene, aa]))
+        elif 'ANN' in rec.info:
+            ann = rec.info['ANN'][0].split('|')
+            # print(ann)
+            consequence = ann[1]
+            gene = ann[3]
+            protein = ann[10][2:]
+            output.append(consequence)
+            output.append(gene)
+            output.append(protein)
+            if protein != '':
+                aa = '-'.join(map(str, [gene, protein]))
+            else:
+                aa = 'NA'
+            output.append(aa)
+        else:
+            output.append('')
+        
+        print('\t'.join(map(str, output)))
+    
+
+if __name__ == '__main__':
+    main()
